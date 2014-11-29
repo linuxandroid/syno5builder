f621caa982651192a5b2ac31ba72d658989d04c6
steven <steven@geniatech.com>
2014-11-29 12:55:43 +0800

    Build: set output directory

--- build.sh
+++ build.sh
@@ -24,20 +24,20 @@ cd ..
 
 # build img with grub boot
 echo -e "\033[41;37m Building the USB Image with grub ... \033[0m"
-cp __s54493pre_usb.img S54493_grub.img
-sudo mount -o loop,offset=32256 S54493_grub.img /mnt/img
+cp __s54493pre_usb.img ./out/S54493_grub.img
+sudo mount -o loop,offset=32256 ./out/S54493_grub.img /mnt/img
 sudo cp kernel/arch/x86_64/boot/bzImage /mnt/img/zImage
 sudo umount /mnt/img
 
 # build iso with grub boot
 echo -e "\033[41;37m Build the ISO with grub ... \033[0m"
 cp kernel/arch/x86_64/boot/bzImage __s54493pre_iso_grub/zImage
-mkisofs -R -J -input-charset UTF-8 -no-emul-boot -boot-load-size 4 -hide-joliet boot.catalog -boot-info-table -b grldr -hide-joliet grldr -o S54493_grub.iso __s54493pre_iso_grub/  
+mkisofs -R -J -input-charset UTF-8 -no-emul-boot -boot-load-size 4 -hide-joliet boot.catalog -boot-info-table -b grldr -hide-joliet grldr -o ./out/S54493_grub.iso __s54493pre_iso_grub/  
 
 #build ios with syslinux boot
 echo -e "\033[41;37m Building the ISO with syslinux ... \033[0m"
 cp kernel/arch/x86_64/boot/bzImage __s54493pre_iso_syslinux/zImage
-mkisofs -R -J -input-charset UTF-8 -no-emul-boot -boot-load-size 4 -hide-joliet boot.catalog -boot-info-table -b bootsec -hide-joliet bootsec -o S54493_syslinux.iso __s54493pre_iso_syslinux/
+mkisofs -R -J -input-charset UTF-8 -no-emul-boot -boot-load-size 4 -hide-joliet boot.catalog -boot-info-table -b bootsec -hide-joliet bootsec -o ./out/S54493_syslinux.iso __s54493pre_iso_syslinux/
 
 # finished
 echo -e "\033[41;37m Everything is ok, burn the img file to you USB storage. \033[0m"
